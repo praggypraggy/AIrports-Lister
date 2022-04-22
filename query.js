b6630920e812fe98696969bcd1d1b94bb9cea24f
praggypraggy <praggypraggy@gmail.com>
2022-04-22 14:01:25 +0530

    Country wise airports cluster

--- query.js
+++ query.js
@@ -1,40 +1,55 @@
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-myHeaders.append("Sec-Fetch-Mode", "cors");
+function bruh(idCode,key,value) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+  myHeaders.append("Sec-Fetch-Mode", "cors");
 
-var urlencoded = new URLSearchParams();
-urlencoded.append(
-  "data",
-  `[out:json];
-  area(id:3600304716)->.searchArea;
+  var urlencoded = new URLSearchParams();
+  urlencoded.append(
+    "data",
+    `[out:json];
+    area(id:`+String(idCode)+`)->.searchArea;
+    (
+      node["`+ key +`"="`+ value +`"](area.searchArea);
+      way["`+ key +`"="`+ value +`"](area.searchArea);
+      relation["`+ key +`"="`+ value +`"](area.searchArea);
+    );
+    out body;
+    >;
+    out skel qt;`
+  );
+
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: 'follow'
+  };
+
+  let resOuter;
+  let prom = new Promise((res, rej) => {
+    resOuter = res;
+  })
+
+  console.log("calling API");
+  
+  console.log(`[out:json];
+  area(id:`+String(idCode)+`)->.searchArea;
   (
-    node["aeroway"="terminal"](area.searchArea);
-    way["aeroway"="terminal"](area.searchArea);
-    relation["aeroway"="terminal"](area.searchArea);
+    node["`+ key +`"="`+ value +`"](area.searchArea);
+    way["`+ key +`"="`+ value +`"](area.searchArea);
+    relation["`+ key +`"="`+ value +`"](area.searchArea);
   );
   out body;
   >;
-  out skel qt;`
-);
+  out skel qt;`);
+  fetch("https://overpass-api.de/api/interpreter", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+      console.log("All data nodes fetched!");
+      resOuter(JSON.parse(result));
+    })
+    .catch(error => console.log('error', error));
+  return prom;
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: urlencoded,
-  redirect: 'follow'
-};
-
-var cod;
-
-
-// fetch("https://overpass-api.de/api/interpreter", requestOptions)
-//   .then(response => response.text())
-//   .then(result => {
-//     cod = result;
-//     console.log("result")
-//   })
-//   .catch(error => console.log('error', error));
-function bruh() {
-  return requestOptions;
-  
 }
