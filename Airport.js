45f1cb4824bfc29f20dd3e89c37d70a93d4ebbac
praggypraggy <praggypraggy@gmail.com>
2022-04-29 17:13:05 +0530

    Changed marker icon,Added marker info and changed Favicon

--- Airport.js
+++ Airport.js
@@ -40,7 +40,7 @@ function printAirportTable(countryName, features) {
         `<br>` +
         features[i].properties.name;
     apnd.innerHTML += `
-    <tr onclick="showAirport(features[`+ i + `].geometry.coordinates)">
+    <tr onclick="showAirport(features[`+ i + `])">
       <td>`+ parseInt(i + 1) + `</td>
       <td class='countryName airportName'>
         `+ airportName + `
@@ -98,58 +98,3 @@ function closeloading() {
   console.log('closing loading');
 }
 
-function showAirports(country, num) {
-  var map = new ZMap('MapdivId', zMapsSdk);
-  map.loadMap();
-  map.onLoad(async () => {
-    openloading('fetching nodes...');
-    var Markers = [];
-    var latLonArray = [];
-    var idCode;
-    console.log(airportData[num], num, country)
-    features = airportData[num].features;
-    console.log(features);
-    await delay(1000);
-    printAirportTable(country, features);
-    for (var i = 0; i < features.length; i++) {
-      if (features[i].geometry.type === "Point") {
-        latLonArray.push([features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]]);
-      }
-      else {
-        latLonArray.push([features[i].geometry.coordinates[0][0][1], features[i].geometry.coordinates[0][0][0]]);
-      }
-    }
-    for (var i = 0; i < latLonArray.length; i++) {
-      Markers[i] = new ZMarker(i, latLonArray[i][0], latLonArray[i][1]);
-      Markers[i].setCustomInfoWindowView('View');
-    }
-    var ZMapsMarkerOptionCluster = new ZMapsMarkerOption(ZMapsMarkerType.Cluster);
-    var response = map.addMarkers('ClusterId', Markers, ZMapsMarkerOptionCluster);
-    closeloading();
-
-  });
-}
-
-function showInZoom(coordinateArray, Marker) {
-
-  var map = new ZMap('MapdivId', zMapsSdk);
-  map.loadMap();
-  map.onLoad(function () {
-
-    // if (Marker !== 'false')
-    map.addMarker(Marker);
-    var zMapsShape = new ZMapsShape('shapeId', ZMapsShapeType.Polygon);
-    zMapsShape.setCoordinatesArray(coordinateArray);
-
-    zMapsShape.setFill(true);
-    zMapsShape.setStrokeColor('black');
-    zMapsShape.setFillColor('rgba(66, 153, 245,0.3)');
-    zMapsShape.setStrokeWidth(2);
-    zMapsShape.setShapeOpacity(1);
-
-    map.addShape(zMapsShape);
-
-    map.fitToShapes(zMapsShape, 20);
-
-  });
-}
